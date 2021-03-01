8ffa43157e4b39015092558068741cb50061df18
wangguocheng <>
2021-03-01 10:49:17 +0800

    日常提交：2021-03-01

--- src/main/java/my/jenny/test/Object2MapTest.java
+++ src/main/java/my/jenny/test/Object2MapTest.java
@@ -1,5 +1,6 @@
 package my.jenny.test;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import my.jenny.finance.entity.po.TransferLog;
@@ -17,10 +18,17 @@ public class Object2MapTest {
         transferLog.setTransferType(1);
         transferLog.setTransferTime(new Date());
         transferLog.setAmount(new BigDecimal("2310.00"));
+        // 方式一：jackson
         // 将object转成map
         Map<String, Object> map = new ObjectMapper().convertValue(transferLog, Map.class);
         // 转出转换结果
         System.out.println("## 转换结果 ##");
         System.out.println(new Gson().toJson(map));
+        // 方式二：hutool
+        Map<String, Object> hutoolMap = BeanUtil.beanToMap(transferLog);
+        // 转出转换结果
+        System.out.println("## 转换结果 ##");
+        System.out.println(new Gson().toJson(hutoolMap));
+
     }
 }
